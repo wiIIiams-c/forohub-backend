c09ab9f3afb72029e71a1ee0103f24a87d35fe94
Williams C <wilcontreras.developer@gmail.com>
2024-06-05 22:10:04 -0400

    Init Repo

---
+++ src/main/java/challenge/backend/forohub/api/forohub_backend/ForohubBackendApplication.java
@@ -0,0 +1,13 @@
+package challenge.backend.forohub.api.forohub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForohubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForohubBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/challenge/backend/forohub/api/forohub_backend/ForohubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.backend.forohub.api.forohub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForohubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
