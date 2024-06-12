34d4b4f9711bf160a9ee4003a8303cc6f3541c65
Williams C <wilcontreras.developer@gmail.com>
2024-06-11 22:20:51 -0400

    Update UserRepository

--- src/main/java/challenge/backend/forohub/api/forohub_backend/domain/user/UserRepository.java
+++ src/main/java/challenge/backend/forohub/api/forohub_backend/domain/user/UserRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long>{
-
     UserDetails findByEmail(String email);
     Boolean existsByEmail(String email);
-
 }
