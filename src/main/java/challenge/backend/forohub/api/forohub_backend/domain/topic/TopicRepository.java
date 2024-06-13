67f46b7b154155558ab130ac12e981cc4bab7304
Williams C <wilcontreras.developer@gmail.com>
2024-06-12 22:34:34 -0400

    Update topic repository
    se agregan las querys que van a consultar si los campos titulo y mensaje ya existen estos se devuelven como string

--- src/main/java/challenge/backend/forohub/api/forohub_backend/domain/topic/TopicRepository.java
+++ src/main/java/challenge/backend/forohub/api/forohub_backend/domain/topic/TopicRepository.java
@@ -1,5 +1,22 @@
 package challenge.backend.forohub.api.forohub_backend.domain.topic;
 
-public interface TopicRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface TopicRepository extends JpaRepository<Topic, Long>{
+
+        @Query("""
+                SELECT t.title
+                FROM Topic t
+                WHERE REPLACE(t.title, " ", "") = :title
+                """)
+        String findByTitle(String title);
+
+        @Query("""
+                SELECT t.message
+                FROM Topic t
+                WHERE REPLACE(t.message, " ", "") = :message
+                """)
+        String findByMessage(String message);
 
 }
