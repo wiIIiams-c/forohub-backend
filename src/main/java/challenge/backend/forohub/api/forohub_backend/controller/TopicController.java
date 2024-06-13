f0db9c7a5d10b292a9fbe91e4ac4640290524ab9
Williams C <wilcontreras.developer@gmail.com>
2024-06-12 22:30:19 -0400

    Update topic controller
    se agrega el autowired del servicio topic
    
    agregado el newTopic como POST el cual envia los datos al servicio para validaciones correspondientes

--- src/main/java/challenge/backend/forohub/api/forohub_backend/controller/TopicController.java
+++ src/main/java/challenge/backend/forohub/api/forohub_backend/controller/TopicController.java
@@ -1,10 +1,32 @@
 package challenge.backend.forohub.api.forohub_backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import challenge.backend.forohub.api.forohub_backend.domain.services.TopicService;
+import challenge.backend.forohub.api.forohub_backend.domain.topic.DataNewTopic;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/topics")
 public class TopicController {
+    private TopicService topicService;
+    
+    @Autowired
+    public TopicController(TopicService topicService) {
+        this.topicService = topicService;
+    }
 
+    @PostMapping
+    @Transactional
+    public ResponseEntity newTopic(@RequestBody @Valid DataNewTopic dataNewTopic){
+        var responseNewTopic = topicService.addNewTopic(dataNewTopic);
+
+        return ResponseEntity.ok(responseNewTopic);
+    }
 }
