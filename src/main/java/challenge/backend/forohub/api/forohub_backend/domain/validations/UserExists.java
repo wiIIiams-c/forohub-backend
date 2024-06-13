9175524f7220790549fbf470d27769cab5928feb
Williams C <wilcontreras.developer@gmail.com>
2024-06-12 22:31:39 -0400

    Cambio de nombre

--- src/main/java/challenge/backend/forohub/api/forohub_backend/domain/validations/UserExists.java
+++ src/main/java/challenge/backend/forohub/api/forohub_backend/domain/validations/UserExists.java
@@ -3,7 +3,7 @@ package challenge.backend.forohub.api.forohub_backend.domain.validations;
 import challenge.backend.forohub.api.forohub_backend.domain.user.DataAuthenticationUser;
 import challenge.backend.forohub.api.forohub_backend.domain.user.UserRepository;
 
-public class ValidationUserExists {
+public class UserExists {
     private UserRepository userRepository;
 
     public void isUserExists(DataAuthenticationUser dataAuthUser){
