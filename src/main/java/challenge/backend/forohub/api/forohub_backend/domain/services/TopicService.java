cbfac95e18331762e8482901c7e750ff7b0eab54
Williams C <wilcontreras.developer@gmail.com>
2024-06-13 21:46:00 -0400

    Update
    * refactorizacion de nombre del record DataNewTopic
    
    * se deja funcionando el actualizar, solo falta validar que el usuario enviado sea el mismo que actualiza el topic

--- src/main/java/challenge/backend/forohub/api/forohub_backend/domain/services/TopicService.java
+++ src/main/java/challenge/backend/forohub/api/forohub_backend/domain/services/TopicService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 import challenge.backend.forohub.api.forohub_backend.domain.course.Course;
 import challenge.backend.forohub.api.forohub_backend.domain.course.CourseRepository;
-import challenge.backend.forohub.api.forohub_backend.domain.topic.DataNewTopic;
+import challenge.backend.forohub.api.forohub_backend.domain.topic.DataTopic;
 import challenge.backend.forohub.api.forohub_backend.domain.topic.DataSavedTopic;
 import challenge.backend.forohub.api.forohub_backend.domain.topic.Topic;
 import challenge.backend.forohub.api.forohub_backend.domain.topic.TopicRepository;
@@ -16,6 +16,7 @@ import challenge.backend.forohub.api.forohub_backend.domain.user.UserEntity;
 import challenge.backend.forohub.api.forohub_backend.domain.user.UserRepository;
 import challenge.backend.forohub.api.forohub_backend.domain.validations.TopicValidations;
 import challenge.backend.forohub.api.forohub_backend.infra.errors.IntegrityValidation;
+import jakarta.validation.Valid;
 
 @Service
 public class TopicService {
@@ -33,10 +34,12 @@ public class TopicService {
         this.topicValidations = topicValidations;
     }
 
-    public DataSavedTopic addNewTopic(DataNewTopic dataNewTopic) {
+    public DataSavedTopic addNewTopic(DataTopic dataNewTopic) {
+        //que el curso y el usuario exista
         var methodCourse = getSelectedCourse(dataNewTopic);
         var methodUser = getSelectedUser(dataNewTopic);
         
+        //que los titulos y mensajes no sean iguales
         topicValidations.forEach(tv -> tv.validateTopic(dataNewTopic));
 
         var currentDate = LocalDateTime.now();        
@@ -53,7 +56,28 @@ public class TopicService {
         return new DataSavedTopic(newTopic);
     }
 
-    private Course getSelectedCourse(DataNewTopic data){
+    public Topic updateTopic(Long id, @Valid DataTopic dataUpdateTopic) {
+        //que no venga nada null -> ya lo valida en el record DataTopic
+        //exista el id
+        if(!topicRepository.findById(id).isPresent()){
+            throw new IntegrityValidation("El topic a actualizar no ha sido encontrado...");
+        }
+        
+        //que los titulos y mensajes no sean iguales
+        topicValidations.forEach(tv -> tv.validateTopic(dataUpdateTopic));
+
+        //que el curso y el usuario exista
+        var methodCourse = getSelectedCourse(dataUpdateTopic);
+        var methodUser = getSelectedUser(dataUpdateTopic);
+
+        //actualizar
+        Topic updateTopic = topicRepository.getReferenceById(id);
+        updateTopic.updateTopic(dataUpdateTopic, methodCourse);
+
+        return updateTopic;
+    }
+
+    private Course getSelectedCourse(DataTopic data){
         if(!courseRepository.findById(data.course()).isPresent()){
             throw new IntegrityValidation("El curso ingresado es invalido...");
         }
@@ -61,7 +85,7 @@ public class TopicService {
         return courseRepository.getReferenceById(data.course());
     }
 
-    private UserEntity getSelectedUser(DataNewTopic data){
+    private UserEntity getSelectedUser(DataTopic data){
         if(!userRepository.findById(data.author()).isPresent()){
             throw new IntegrityValidation("El usuario ingresado es invalido...");
         }
