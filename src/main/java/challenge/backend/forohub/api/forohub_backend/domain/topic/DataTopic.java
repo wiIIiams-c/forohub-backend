ed4997b20a6665dd1ceda271294c71466e836881
Williams C <wilcontreras.developer@gmail.com>
2024-06-13 21:43:39 -0400

    Cambio de nombre
    se cambia de nombre para dejarlo de forma general ya que se ocupa para agregar o actualizar un topico

--- src/main/java/challenge/backend/forohub/api/forohub_backend/domain/topic/DataTopic.java
+++ src/main/java/challenge/backend/forohub/api/forohub_backend/domain/topic/DataTopic.java
@@ -3,7 +3,7 @@ package challenge.backend.forohub.api.forohub_backend.domain.topic;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record DataNewTopic(
+public record DataTopic(
     @NotBlank String title,
     @NotBlank String message,
     @NotNull Long author,
