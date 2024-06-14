44f678fdc7acf0f1ae7aac888868a602ef620f10
Williams C <wilcontreras.developer@gmail.com>
2024-06-13 21:44:18 -0400

    Update
    refactorizacion de nombre

--- src/main/java/challenge/backend/forohub/api/forohub_backend/controller/TopicController.java
+++ src/main/java/challenge/backend/forohub/api/forohub_backend/controller/TopicController.java
@@ -2,13 +2,16 @@ package challenge.backend.forohub.api.forohub_backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import challenge.backend.forohub.api.forohub_backend.domain.services.TopicService;
-import challenge.backend.forohub.api.forohub_backend.domain.topic.DataNewTopic;
+import challenge.backend.forohub.api.forohub_backend.domain.topic.DataSavedTopic;
+import challenge.backend.forohub.api.forohub_backend.domain.topic.DataTopic;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 
@@ -24,9 +27,17 @@ public class TopicController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity newTopic(@RequestBody @Valid DataNewTopic dataNewTopic){
+    public ResponseEntity topicNew(@RequestBody @Valid DataTopic dataNewTopic){
         var responseNewTopic = topicService.addNewTopic(dataNewTopic);
 
         return ResponseEntity.ok(responseNewTopic);
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity topicUpdate(@PathVariable Long id, @RequestBody @Valid DataTopic dataUpdateTopic){
+        var updatedTopic = topicService.updateTopic(id, dataUpdateTopic);
+
+        return ResponseEntity.ok(new DataSavedTopic(updatedTopic));
     }
 }
