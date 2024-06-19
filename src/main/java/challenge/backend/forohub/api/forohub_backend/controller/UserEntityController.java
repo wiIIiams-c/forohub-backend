45dead6cec8536c462cdfe5c656f7d9b2f20966b
Williams C <wilcontreras.developer@gmail.com>
2024-06-19 14:45:11 -0400

    Update user controller
    se corrige error de campos vacios al enviar el update

--- src/main/java/challenge/backend/forohub/api/forohub_backend/controller/UserEntityController.java
+++ src/main/java/challenge/backend/forohub/api/forohub_backend/controller/UserEntityController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,7 +40,7 @@ public class UserEntityController {
 
     @PutMapping("/{id}")
     @Transactional
-    public ResponseEntity userUpdate(@PathVariable @Valid Long id, @Valid DataRegisterUser dataRegisterUser){
+    public ResponseEntity userUpdate(@PathVariable @Valid Long id, @RequestBody @Valid DataRegisterUser dataRegisterUser){
         var userResponse = userService.updateUser(id, dataRegisterUser);
 
         return ResponseEntity.ok(userResponse);
