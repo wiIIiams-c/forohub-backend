25486e02267531e70954ed0f1e457a9f3c1bfb41
Williams C <wilcontreras.developer@gmail.com>
2024-06-22 22:24:12 -0400

    Update
    se agrega comentarios

--- src/main/java/challenge/backend/forohub/api/forohub_backend/domain/validations/TopicValidations.java
+++ src/main/java/challenge/backend/forohub/api/forohub_backend/domain/validations/TopicValidations.java
@@ -1,5 +1,10 @@
 package challenge.backend.forohub.api.forohub_backend.domain.validations;
 
+/*
+ * Interface que validara cualquier regla de negocio que se quiera implementar,
+ * esta reciebe cualquier tipo de objeto ya que la clase que la implemente
+ * podra castear a cualquier tipo de objeto y validar los datos de ese objeto.
+ */
 public interface TopicValidations {
     public void validateTopic(Object data);
 }
