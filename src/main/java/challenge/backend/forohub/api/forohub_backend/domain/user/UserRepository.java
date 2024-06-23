2624e71f638012b258bf5f7825fb8aabefd53eb3
Williams C <wilcontreras.developer@gmail.com>
2024-06-22 22:23:25 -0400

    Update
    se agrega query para obtener el id del usuario a partir del nombre

--- src/main/java/challenge/backend/forohub/api/forohub_backend/domain/user/UserRepository.java
+++ src/main/java/challenge/backend/forohub/api/forohub_backend/domain/user/UserRepository.java
@@ -1,9 +1,17 @@
 package challenge.backend.forohub.api.forohub_backend.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long>{
     UserDetails findByEmail(String email);
     Boolean existsByEmail(String email);
+
+    @Query("""
+            SELECT u.id
+            FROM UserEntity u
+            WHERE u.name = :name
+            """)
+    Long findIdByName(String name);
 }
