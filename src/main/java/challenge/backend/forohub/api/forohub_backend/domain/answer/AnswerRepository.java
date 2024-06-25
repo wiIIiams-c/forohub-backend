887948c4002e9adf2ee255abf743add75f0a2f8e
Williams C <wilcontreras.developer@gmail.com>
2024-06-25 14:27:29 -0400

    Update repository
    se agrega un optional que consulta un topico con el mensaje a responder

--- src/main/java/challenge/backend/forohub/api/forohub_backend/domain/answer/AnswerRepository.java
+++ src/main/java/challenge/backend/forohub/api/forohub_backend/domain/answer/AnswerRepository.java
@@ -1,7 +1,18 @@
 package challenge.backend.forohub.api.forohub_backend.domain.answer;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long>{
-
+    @Query("""
+            SELECT a
+            FROM Answer a
+            WHERE a.topic.id = :idTopic
+            AND a.answer = :message
+            """)
+    Optional<Answer> findByTopicMessage(Long idTopic, String message);
 }
