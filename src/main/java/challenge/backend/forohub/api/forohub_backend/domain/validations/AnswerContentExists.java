355e86e81dbeddd6c8d1378600069399edd17459
Williams C <wilcontreras.developer@gmail.com>
2024-07-01 14:34:19 -0400

    Update answer content exist
    se actualiza el tipo de objeto que recibe ya que puede recibir varios

--- src/main/java/challenge/backend/forohub/api/forohub_backend/domain/validations/AnswerContentExists.java
+++ src/main/java/challenge/backend/forohub/api/forohub_backend/domain/validations/AnswerContentExists.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 import challenge.backend.forohub.api.forohub_backend.domain.answer.AnswerRepository;
 import challenge.backend.forohub.api.forohub_backend.domain.answer.DataAnswer;
+import challenge.backend.forohub.api.forohub_backend.domain.answer.DataAnswerUpdate;
 import challenge.backend.forohub.api.forohub_backend.infra.errors.IntegrityValidation;
 
 @Component
@@ -14,19 +15,26 @@ public class AnswerContentExists implements AnswerValidations{
 
     @Override
     public void validateAnswer(Object data) {
+        Long idTopic = null;
+        String message = null;
+
         if(data instanceof DataAnswer){
-            DataAnswer dataAnswer = (DataAnswer) data;
+            var dataAnswer = (DataAnswer) data;
+            idTopic = dataAnswer.topic();
+            message = dataAnswer.message();
+        }else if(data instanceof DataAnswerUpdate){
+            var dataAnswerUpdate = (DataAnswerUpdate) data;
+            var idAnswer = dataAnswerUpdate.idAnswer();
+            var answerData = answerRepository.getReferenceById(idAnswer);
+            idTopic = answerData.getTopic().getId();
+            message = dataAnswerUpdate.newMessage();
+        }
 
-            if(dataAnswer.author() == null || dataAnswer.topic() == null || dataAnswer.message() == null){
-                return;
-            }
+        //validar que el mensaje no exista para el topico
+        var messageExist = answerRepository.findByTopicMessage(idTopic, message);
 
-            //validar que el mensaje no exista para el topico
-            var messageExist = answerRepository.findByTopicMessage(dataAnswer.topic(), dataAnswer.message());
-
-            if(messageExist.isPresent()){
-                throw new IntegrityValidation("La respuesta ya existe para el topico...");
-            }
+        if(messageExist.isPresent()){
+            throw new IntegrityValidation("La respuesta ya existe para el topico...");
         }
     }
 
