6d3b206927515941c20cdcef20b46e28d167677e
Williams C <wilcontreras.developer@gmail.com>
2024-07-05 20:33:03 -0400

    Update
    se agrega la opcion de listas de servidor para el spring doc / por confirmar

--- src/main/java/challenge/backend/forohub/api/forohub_backend/infra/springdoc/SpringDocConfiguration.java
+++ src/main/java/challenge/backend/forohub/api/forohub_backend/infra/springdoc/SpringDocConfiguration.java
@@ -1,17 +1,24 @@
 package challenge.backend.forohub.api.forohub_backend.infra.springdoc;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import jakarta.servlet.ServletContext;
 
 @Configuration
 public class SpringDocConfiguration {
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI customOpenAPI(ServletContext servletContext) {
+        Server server = new Server().url(servletContext.getContextPath());
+        
         return new OpenAPI()
+                .servers(List.of(server))
                 .components(new Components()
                 .addSecuritySchemes("bearer-key",
                 new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
